6ea7d6bb11f810064f5c4e0647a9406023852c81
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-11-23 21:53:32 +0200

    function basics

---
+++ func.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+)
+
+func main() {
+    module := "function basics"
+    author := "muzikayise buthelezi"
+
+    fmt.Println(converter(module, author))
+}
+
+func converter (s1, s2 string) (str1, str2 string) {
+
+    s1 = strings.Title(s1)
+    s2 = strings.ToUpper(s2)
+
+    return s1, s2
+}
