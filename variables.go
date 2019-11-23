291a1b694ee64ecd4e7991f019dad102c908195d
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-11-23 21:16:13 +0200

    ref using & and de-ref using *

--- variables.go
+++ variables.go
@@ -5,12 +5,16 @@ import (
     "reflect"
 )
 
-var (
-    name, course, module = "Muzi", "Golang", 1.5
-)
-
 func main() {
+
+    name := "Muzi"
+//     course := "Golang"
+    module := 1.5
+    ptr := &module
+
     fmt.Println("Name is set to", name, "and is of type", reflect.TypeOf(name))
     fmt.Println("Module is set to", module, "and is of type", reflect.TypeOf(module))
-    fmt.Println("Course is set to", course)
+    fmt.Println("Memory address of *module* variable is",
+    ptr, "and the value of *module*", *ptr)
+//     fmt.Println("Course is set to", course)
 }
