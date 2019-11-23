39f26d37f032461f669445e8aae599d154dbc65e
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-11-23 22:01:50 +0200

    variadic function

---
+++ func1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    bestFinish := bestLeagueFinishes(13, 10, 13, 17, 14, 16, 6, 7, 3, 4, 1)
+    fmt.Println(bestFinish)
+}
+
+func bestLeagueFinishes(finishes ...int) int {
+
+    best := finishes[0]
+    for _, i := range finishes {
+        if i < best {
+            best = i
+        }
+    }
+
+    return best
+}
