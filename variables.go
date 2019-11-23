3241169b3421d404046a6f24b240bd93bca06314
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-11-23 21:25:10 +0200

    updating varibale using ref and de-ref

--- variables.go
+++ variables.go
@@ -2,19 +2,24 @@ package main
 
 import (
     "fmt"
-    "reflect"
 )
 
 func main() {
 
     name := "Muzi"
-//     course := "Golang"
-    module := 1.5
-    ptr := &module
+    course := "Golang"
 
-    fmt.Println("Name is set to", name, "and is of type", reflect.TypeOf(name))
-    fmt.Println("Module is set to", module, "and is of type", reflect.TypeOf(module))
-    fmt.Println("Memory address of *module* variable is",
-    ptr, "and the value of *module*", *ptr)
-//     fmt.Println("Course is set to", course)
+    fmt.Println("\nHi", name, "you're currently watching", course)
+
+    changeCourse(&course)
+
+    fmt.Println("\nYou are now watching course", course)
+}
+
+func changeCourse(course *string) string {
+    *course = "First Look: Native Docker Clustering"
+
+    fmt.Println("\nTrying to changer your course to", *course)
+
+    return *course
 }
