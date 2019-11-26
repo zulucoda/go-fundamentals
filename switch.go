77a13e96d25385b3c10dc687a8dbb7d5ae523e57
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-11-26 20:40:53 +0200

    more switch example

--- switch.go
+++ switch.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "fmt"
+    "math/rand"
+    "time"
 )
 
 func main() {
@@ -15,4 +17,23 @@ func main() {
         fmt.Println("Sorry no match found....")
     }
 
+
+    main2()
+
+}
+
+func main2() {
+
+    switch tmpNum := random(); tmpNum {
+     case 0,2,4,6,8:
+        fmt.Println("We got an even number", tmpNum)
+     case 1,3,5,7,9:
+        fmt.Println("We got an odd number", tmpNum)
+    }
+
+}
+
+func random() int {
+    rand.Seed(time.Now().Unix())
+    return rand.Intn(10)
 }
