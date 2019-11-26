35b323294c48d5676c37bf4e043256af50c8ac86
Muzikayise Flynn Buthelezi (zulucoda) <muzi@mfbproject.co.za>
2019-11-26 20:30:40 +0200

    if simple statements

--- if.go
+++ if.go
@@ -17,4 +17,18 @@ func main() {
         fmt.Println("Both course are same")
     }
 
+    main2()
+
+}
+
+func main2() {
+
+    if firstRank, secondRank := "614","614" ;  firstRank < secondRank {
+        fmt.Println("First course is doing better than the second course")
+    } else if firstRank > secondRank {
+        fmt.Println("oh.... your first course is not doing well")
+    } else {
+        fmt.Println("Both course are same")
+    }
+
 }
