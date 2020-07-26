94fd7d0d1f73de62f31cce7e3c65721cdc22f980
Muzikayise Flynn Buthelezi (zuluCoda) <muzi@mfbproject.co.za>
2020-07-26 21:29:19 +0200

    appending slices and slice values

---
+++ slices-misc.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	mySlice := []int{1, 2, 3, 4, 5}
+	fmt.Println(mySlice)
+
+	// _ is index pos
+	// i is data
+	for _, i := range mySlice {
+		fmt.Println("for range loop:", i)
+	}
+
+	newSlice := []int{10, 20, 30}
+	mySlice = append(mySlice, newSlice...)
+	fmt.Println(mySlice)
+}
